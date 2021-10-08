30d8bf14d2bfda10100f7d965f867969480dc2e4
drdhaval2785 <drdhaval2785@gmail.com>
2021-10-08 11:25:27 +0530

    indent wordwise

--- scripts/indent.py
+++ scripts/indent.py
@@ -27,12 +27,32 @@ def insert_tabs(entry, t='\t', l='\n'):
 	entry = re.sub('\[({.*?)\] ', l + t*3 + '[\g<1>] ' + l + t*4 , entry)
 	entry = re.sub('({@\-\-[0-9])', l + t*4 + '\g<1>', entry)
 	entry = entry.replace('<P>', l + t*4 + '<P>')
-	entry = re.sub('({#\-\-.*?#} )({%<ab>.*</ab>%} )([^\[])', '\g<1>' + l + t*3 + '\g<2>' + l + t*4 + '\g<3>', entry)
 	# Verb section
 	entry = re.sub('( {[cv].*?</ab> )({#.*?#} )', l + t + '\g<1>' + l + t*2 + '\g<2>' + l + t*3, entry)
 	return entry
 
 
+def insert_tabs1(entry, l='\n', t='\t'):
+	words = re.split('([ ]+)', entry)
+	result = ''
+	tlevel = 0
+	compound = False
+	verb = False
+	for word in words:
+		if re.search('{#\-\-.*?#}', word):
+			result += l + t*2 + word
+		elif '{%--<ab>' in word:
+			result += l + t*2 + word
+		elif re.search('<P>', word):
+			result += word.replace('<P>', l + t*4 + '<P>')
+		elif re.search('^\[', word) and not re.search('^\[Page', word):
+			result += l + t*3 + word
+		elif re.search('{@[0-9\-]+@}', word):
+			result += l + t*4 + word
+		else:
+			result += word
+	return result
+	
 def adjust_text(filein, fileout):
 	fin = codecs.open(filein, 'r', 'utf-8')
 	fout = codecs.open(fileout, 'w', 'utf-8')
@@ -43,7 +63,8 @@ def adjust_text(filein, fileout):
 			entry = ''
 		elif lin.startswith('<LEND>'):
 			entry = remove_line_breaks(entry)
-			entry = insert_tabs(entry)
+			#entry = insert_tabs(entry)
+			entry = insert_tabs1(entry)
 			fout.write(entry + '\n' + lin)
 		else:
 			entry += lin
